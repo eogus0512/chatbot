459a9e29655c3cd7ca5bfad74c77f5bbb379ab26
eogus0512 <71585151+eogus0512@users.noreply.github.com>
2021-01-10 01:13:04 +0900

    connect DB

--- train_tools/qna/DatabaseConfig.py
+++ train_tools/qna/DatabaseConfig.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 DB_HOST = "localhost"
 DB_USER = "root"
 DB_PASSWORD = "tjwjdeogus369!"
@@ -5,3 +11,4 @@ DB_NAME = "homestead"
 
 def DatabaseConfig():
     global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+
